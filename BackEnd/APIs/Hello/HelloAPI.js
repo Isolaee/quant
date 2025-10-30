16e2e97f5b50f37288417a4bbd6924579955db34
isolaee <eero.isola@gmail.com>
2025-10-30 08:58:12 +0200

    Imporved commentation fro clarity.

--- BackEnd/APIs/Hello/HelloAPI.js
+++ BackEnd/APIs/Hello/HelloAPI.js
@@ -1,6 +1,10 @@
+// This module publishes a simple Hello API endpoint
+// Endpoints are developed and published as individual modules for modularity
+
 const express = require('express');
 const router = express.Router();
 
+// This endpoint responds to GET requests with a message
 router.get('/', (req, res) => {
   res.json({ message: 'Hello from Node server!' });
 });
