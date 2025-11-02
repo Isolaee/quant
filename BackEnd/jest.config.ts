d23a60dfb5e973c65cf4652f4c87ff68dca7a39c
isolaee <eero.isola@gmail.com>
2025-11-02 18:46:33 +0200

    Added jest config

---
+++ BackEnd/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
